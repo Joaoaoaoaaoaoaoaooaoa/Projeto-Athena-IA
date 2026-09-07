a600060f88275e335ae1d07951b96eae0ac4d285
Joaoaoaoaaoaoaoaooaoa <joao62217116@alunos.sc.senac.br>
2026-09-07 20:32:47 -0300

    Delete user (12).py

--- user (12).py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel, EmailStr
-
-class UserResponse(BaseModel):
-    id: int
-    name: str
-    email: EmailStr
-    active: bool
-    model_config = {"from_attributes": True}
